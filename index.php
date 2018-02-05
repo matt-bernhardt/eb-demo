587bf90a9bba502d4588bbe249971959606ef150
Matt Bernhardt <mjbernha@mit.edu>
2018-02-05 13:47:13 -0500

    General cleanup

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 </head>
 <body>
     <section class="congratulations">
-        <h1>Congratulations!!!</h1>
+        <h1>Congratulations!!!!!</h1>
         <p>Your AWS Elastic Beanstalk <em>PHP</em> application is now running on your own dedicated environment in the AWS&nbsp;Cloud</p>
         <p>You are running PHP version <?php echo phpversion(); ?></p>
     </section>
